c820d51ba9ee7dc638ca0da9f92cf50599fbf110
Thomas Propson <tcpropson@gmail.com>
2018-12-17 15:31:45 -0600

    updated models for new application questions

--- theapplication/forms.py
+++ theapplication/forms.py
@@ -9,18 +9,14 @@ RACE = (('W', 'White'),
         ('H', 'Hispanic or Latino'))
 
 class ApplicationForm(forms.ModelForm):
-    is_adult = forms.BooleanField()
-    inforelease = forms.BooleanField()
-    terms_conditions = forms.BooleanField()
-    code_of_conduct = forms.BooleanField()
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
 
     class Meta:
         model = Application
-        fields = ['school', 'grad_year', 'pronouns', 'gender', 'race', 'major', 'hackathons', 'essay1', 'essay2', 'essay3', 'essay4', 'essay5', 'proudof', 'reimbursement', 'location']
+        fields = '__all__'
 
 class ConfirmationForm(forms.ModelForm):
     class Meta:
         model = Confirmation
-        fields = ['phone_number', 'dietary_restrictions', 'shirt_size', 'notes']
+        fields = '__all__'
--- theapplication/models.py
+++ theapplication/models.py
@@ -7,13 +7,63 @@ class RaceChoice(models.Model):
 
     def __str__(self):
         return self.race_string
-# application class, to attach to applicant class
-class Application(models.Model):
-    # what's their school
-    # TODO: make this a selector not a text field
-    school = models.CharField(max_length=200)
 
-    # what's graduation year?
+# Application class, to attach to applicant class.
+class Application(models.Model):
+
+    # TODO: this needs validation
+    phone_number = models.CharField(
+                    max_length=50,
+                    null=True,
+    )
+
+    # TODO: this needs validation
+    birth_date = models.DateField(
+                    null=True,
+    )
+
+    GENDER_CHOICES = (
+        ('M_', 'Male'),
+        ('NB', 'Nonbinary'),
+        ('F_', 'Female'),
+        ('or', 'Other/Prefer not to disclose'),
+    )
+
+    gender = models.CharField(
+            max_length=2,
+            choices=GENDER_CHOICES,
+            default='or',
+    )
+
+    pronouns = models.CharField(
+                max_length=150,
+                verbose_name="What are your pronouns?",
+                null=True,
+    )
+
+    race = models.ManyToManyField(
+            RaceChoice,
+            verbose_name="What is your race/ethnicity?",
+    )
+
+    # TODO: make this a selector not a text field
+    school = models.CharField(
+                max_length=200,
+                null=True,
+    )
+
+    major = models.CharField(
+                max_length=150,
+                verbose_name="What is your major?",
+                null=True,
+    )
+
+    study_level = models.CharField(
+                    max_length=50,
+                    verbose_name="What is your most current level of study?",
+                    null=True
+    )
+
     # Constants in Model class
     YEAR_IN_SCHOOL_CHOICES = (
         ('19', '2019'),
@@ -23,56 +73,105 @@ class Application(models.Model):
         ('23', '2023 or Later'),
         ('or', 'other'),
     )
+
     grad_year = models.CharField(
             max_length=2,
             choices=YEAR_IN_SCHOOL_CHOICES,
-            default='or'
-    )
-    
-    # pronouns text field
-    pronouns = models.CharField(max_length=150)
-    
-    
-    GENDER_CHOICES = (
-        ('M_', 'Male'),
-        ('NB', 'Nonbinary'),
-        ('F_', 'Female'),
-        ('or', 'Other/Prefer not to disclose'),
-    )
-    gender = models.CharField(
-            max_length=2,
-            choices=GENDER_CHOICES,
             default='or',
     )
 
-    major = models.CharField(max_length=150)
-    
-    # race
-    race = models.ManyToManyField(RaceChoice)
+    # TODO: add helper text - city/state/country
+    location = models.CharField(
+                max_length=200,
+                verbose_name="Where are you coming from to attend Uncommon Hacks?",
+                null=True,
+    )
 
-    # prior hackathons TODO make this a selector
-    hackathons = models.CharField(max_length=150)
+    # TODO: add helper text - list names of hackathons
+    # TODO: make this a selector - low priority
+    hackathons = models.CharField(
+                    max_length=150,
+                    verbose_name="How many hackathons have you attended before?",
+                    null=True,
+    )
+
+    self_description = models.CharField(
+                        max_length=1500,
+                        verbose_name="I would describe myself as...",
+                        null=True,
+    )
+
+    # TODO: add helper text - "Github, Devpost, etc."
+    proudof = models.TextField(
+                max_length=1500,
+                verbose_name="Anything you're proud of?",
+                null=True,
+    )    
     
+    # TODO: Figure out where / how we should save this
+    # https://docs.djangoproject.com/en/2.1/ref/models/fields/#filefield
+    resume = models.FileField(
+                null=True,
+    )
+
     # essay questions
-    essay1 = models.TextField(max_length=1500)
-    essay2 = models.TextField(max_length=1500)
-    essay3 = models.TextField(max_length=1500)
-    essay4 = models.TextField(max_length=1500)
-    essay5 = models.TextField(max_length=1500)
+    essay1 = models.TextField(
+                max_length=1500,
+                verbose_name=("There are 2 types of people in this world. "
+                              "One is you. The other is everyone else. "
+                              "What are the types?"),
+                null=True,
+    )
+    essay2 = models.TextField(
+                max_length=1500,
+                verbose_name=("Sandwich (noun): A popular concoction consisting "
+                              "of two or more pieces of surrounding material "
+                              "with various other materials in between them. "
+                              "This definition leads to the very obvious "
+                              "question that I’m sure you’re already thinking "
+                              "of: is an array a sandwich?"),
+                null=True,
+    )
+    essay3 = models.TextField(
+                max_length=1500,
+                verbose_name=("If you could create your own flavor of lacroix, "
+                              "what would it be and why? What would it be "
+                              "called? What would it taste like [to the degree "
+                              "that you can actually taste it ;)]"),
+                null=True,
+    )
+    # TODO: Figure out how to incorporate meme images.
+    essay4 = models.TextField(
+                max_length=1500,
+                verbose_name="Meme fill-in the blanks.",
+                null=True
+    )
 
-    # show us something you're proud of
-    proudof = models.TextField(max_length=1500)
+    # TODO: add helper text
+    # COC: https://static.mlh.io/docs/mlh-code-of-conduct.pdf
+    legal1 = models.BooleanField(
+                default=False,
+                verbose_name="I have read and agree to the MLH Code of Conduct.",
+    )
 
-    # do they need reimbursement
-    reimbursement = models.BooleanField(default=False)
+    # TODO: add helper text
+    # CTC - https://github.com/MLH/mlh-policies/blob/master/prize-terms-and-conditions/contest-terms.md
+    # PP - https://mlh.io/privacy
+    legal2 = models.BooleanField(
+                default=False,
+                verbose_name=("I authorize you to share my application/"
+                              "registration information for event administration"
+                              ", ranking, MLH administration, pre- and post-event"
+                              " informational e-mails, and occasional messages "
+                              "about hackathons in-line with the MLH Privacy "
+                              "Policy. Further, I agree to the terms of both the "
+                              "MLH Contest Terms and Conditions and the MLH "
+                              "Privacy Policy"),
+    )
 
-    # where are they coming from?
-    location = models.CharField(max_length=200)
 
 # confirmation class, to attach to applicant class
 class Confirmation(models.Model):
-    phone_number = models.CharField(max_length=20)
-    
     dietary_restrictions = models.CharField(max_length=1000)
     
     SHIRT_SIZE_CHOICES = (
