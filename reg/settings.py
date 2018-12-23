c144bb1e10a55ef788e8400ebf230b6d2aa7b215
Michael Noronha <michaeltnoronha@gmail.com>
2018-12-23 00:01:20 -0500

    Fix form fields; up to resume seems good

--- reg/settings.py
+++ reg/settings.py
@@ -39,7 +39,7 @@ MAIN_URL = "testing.uncommonhacks.com"
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "71084adc.ngrok.io",
+    "ad6d2be7.ngrok.io",
     MAIN_URL,
     client.get_parameter(Name="django-registration-url")["Parameter"]["Value"],
 ]
