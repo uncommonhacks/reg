37d995e11577f3472e7ce7b647b25cb74a799588
Thomas Propson <tcpropson@gmail.com>
2018-12-23 20:23:15 -0600

    infancy dropdown handling

--- theapplication/static/js/main.js
+++ theapplication/static/js/main.js
@@ -1,11 +1,23 @@
 // Materialize JQuery Initialization
 $(document).ready(function(){
+
   $("select").filter((e) => e != 2).formSelect();
 
   let schoolSelect = $("select[name=school]");
   schoolSelect.select2({width: "100%"});
 
-  $("select").change(function(e) {
-    console.log("selectchagnevent", e);
-  });
+
+  let minimizeDropdowns = function (){
+      console.log("minimizing dropdowns");
+      let selectDropdowns =  Array.prototype.slice.call(document.getElementsByClassName("dropdown-content"), 0);
+      let select2Dropdowns =  Array.prototype.slice.call(document.getElementsByClassName("select2-dropdown"), 0);
+      let dropdowns = selectDropdowns.concat(select2Dropdowns);
+      for(let i = 0; i < dropdowns.length; i++){
+          dropdowns[i].style.display = "none";
+      }
+  };
+
+  document.addEventListener("click", minimizeDropdowns);
+  
+ 
 });
